91f96974705983ccaee78baafbb8555047679452
lamiamostafa <lamiamostafa791@gmail.com>
2022-07-28 01:09:36 +0600

    first css is added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
-        <section>
+        <section class="home-border">
             <div>
-                <h2>Welcome to</h2>
-                <h3> Globetech </h3>
+                <h2 class="header-one">Welcome to</h2>
+                <h3 class="header-two"> Globetech </h3>
                 <p>We are committed to deliver <span>best IT services</span>. Our Consultants have experience in working with clients. We have extensive experience in the software application space and also offer a broad range and depth of technology.</p>
                 <div>
                     <button>Support us</button>
