624da7a22d2c46528f0063fccd689b3ec179f5e7
lamiamostafa <lamiamostafa791@gmail.com>
2022-07-27 22:12:08 +0600

    first commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <section>
+            <div>
+                <h2>Welcome to</h2>
+                <h3> Globetech </h3>
+                <p>We are committed to deliver <span>best IT services</span>. Our Consultants have experience in working with clients. We have extensive experience in the software application space and also offer a broad range and depth of technology.</p>
+                <div>
+                    <button>Support us</button>
+                    <button>Our Mission</button>
+                </div>
+            </div>
+            <div></div>
+        </section>
+    );
+};
+
+export default Home;
